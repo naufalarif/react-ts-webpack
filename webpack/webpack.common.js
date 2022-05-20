4d9b1ac9d6099c4e1b353cf9469646368d4e447b
naufalarif <naufalarif92@gmail.com>
2022-05-20 19:40:45 +0700

    Add bundle analyzer

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, '..', './src/index.tsx'),
@@ -36,5 +37,8 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '..', './src/index.html'),
     }),
+    new CopyPlugin({
+      patterns: [{ from: 'source', to: 'dest' }],
+    }),
   ],
-}
+};
