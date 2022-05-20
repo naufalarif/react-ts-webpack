9c71b08bad7222468aba343c046f227445b0c65b
naufalarif <naufalarif92@gmail.com>
2022-05-20 16:51:51 +0700

    Add husky

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,12 +1,14 @@
-import { FunctionComponent, useState } from "react";
+import { FunctionComponent, useState } from 'react';
 
 const Counter: FunctionComponent = () => {
   const [count, setCount] = useState<number>(0);
   return (
     <div>
-      <button onClick={() => setCount(prevState => prevState + 1)}>Count {count}</button>
+      <button onClick={() => setCount((prevState) => prevState + 1)}>
+        Count {count}
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default Counter;
